d0b4497265d2cea804b9b6f708aa99c08819cd9f
thuccv98 <thuccv98@gmail.com>
2021-07-30 11:34:55 +0700

    Create header,footer and landingpage with react bootstrap

---
+++ frontend/src/screens/LandingPage/LandingPage.js
@@ -0,0 +1,37 @@
+import { Button, Container, Row } from 'react-bootstrap';
+import './LandingPage.css';
+
+const LandingPage = () => {
+  return (
+    <div className="main">
+      <Container>
+        <Row>
+          <div className="intro-text">
+            <div>
+              <h1 className="title">Welcome to Notes App</h1>
+              <p className="subtitle">One Safe place for all your notes.</p>
+            </div>
+            <div className="buttoncontainer">
+              <a href="/login">
+                <Button size="lg" className="landingbutton">
+                  Login
+                </Button>
+              </a>
+              <a href="/register">
+                <Button
+                  size="lg"
+                  className="landingbutton"
+                  variant="outline-primary"
+                >
+                  Signup
+                </Button>
+              </a>
+            </div>
+          </div>
+        </Row>
+      </Container>
+    </div>
+  );
+};
+
+export default LandingPage;
