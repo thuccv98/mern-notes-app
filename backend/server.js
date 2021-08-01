0f57708cca05772410d264a4016c9394a5f6a88c
thuccv98 <thuccv98@gmail.com>
2021-08-01 17:57:52 +0700

    Completed backend side: user authentication with JWT

--- backend/server.js
+++ backend/server.js
@@ -2,25 +2,24 @@ const express = require('express');
 const notes = require('./data/notes');
 const dotenv = require('dotenv');
 const connectDB = require('./config/db');
+const userRoutes = require('./routes/userRoutes');
+const { notFound, errorHandler } = require('./middlewares/errorMiddleware');
 
 const app = express();
 
+//config env
 dotenv.config();
 
+// connect db
 connectDB();
+app.use(express.json());
 
-app.get('/', (req, res) => {
-  res.send('API is running...');
-});
+// use routes
+app.use('/api/users', userRoutes);
 
-app.get('/api/notes', (req, res) => {
-  res.json(notes);
-});
-
-app.get('/api/notes/:id', (req, res) => {
-  const note = notes.find((n) => n._id === req.params.id);
-  res.send(note);
-});
+//middleware handle error
+app.use(notFound);
+app.use(errorHandler);
 
 const PORT = process.env.PORT || 5000;
 
