20746f46843cd96e01a514e7c478cb4689a2b020
thuccv98 <thuccv98@gmail.com>
2021-08-08 16:53:28 +0700

    Deploy to heroku

--- backend/server.js
+++ backend/server.js
@@ -6,6 +6,7 @@ const userRoutes = require('./routes/userRoutes');
 const noteRoutes = require('./routes/noteRoutes');
 
 const { notFound, errorHandler } = require('./middlewares/errorMiddleware');
+const path = require('path');
 
 const app = express();
 
@@ -19,6 +20,23 @@ app.use(express.json());
 // use routes
 app.use('/api/users', userRoutes);
 app.use('/api/notes', noteRoutes);
+
+// ------------Deployment-------------
+
+__dirname = path.resolve();
+if (process.env.NODE_ENV === 'production') {
+  app.use(express.static(path.join(__dirname, '/frontend/build')));
+
+  app.get('*', (req, res) => {
+    res.sendFile(path.resolve(__dirname, 'frontend', 'build', 'index.html'));
+  });
+} else {
+  app.get('/', (req, res) => {
+    res.send('API is running..');
+  });
+}
+
+// ------------Deployment-------------
 
 //middleware handle error
 app.use(notFound);
